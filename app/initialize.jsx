63d5d5f2769d490a473a0d06b961ba255a1ef317
Paul Miller <paul@paulmillr.com>
2021-01-26 22:45:42 +0200

    Update

--- app/initialize.jsx
+++ app/initialize.jsx
@@ -9,14 +9,35 @@ function arrayToHex(array) {
     .join("");
 }
 
+function timer(label) {
+  console.time(label);
+  return (value) => {
+    console.timeEnd(label);
+    return value;
+  }
+}
+
 class ECCCalculator extends React.Component {
   constructor() {
     super();
     this.state = {
       privateKey: '0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef',
-      edPub: '207a067892821e25d770f1fba0c47c11ff4b813e54162ece9eb839e076231ab6',
-      secpPub: '034646ae5047316b4230d0086c8acec687f00b1cd9d1dc634f6cb358ac0a9a8fff',
-      blsPub: '86b50179774296419b7e8375118823ddb06940d9a28ea045ab418c7ecbe6da84d416cb55406eec6393db97ac26e38bd4',
+      secp: {
+        hex: '034646ae5047316b4230d0086c8acec687f00b1cd9d1dc634f6cb358ac0a9a8fff',
+        x: '31786781763520711516504796705501580263047480643491642414438614973346706264063',
+        y: '115098966614433306530954635991961836831388920962654509778142658150360531838297'
+      },
+      ed: {
+        hex: '207a067892821e25d770f1fba0c47c11ff4b813e54162ece9eb839e076231ab6',
+        x: '13413213784859046187686486715039706091158037999095366525547021808119004615937',
+        y: '24471076613190165805373100805867147262687600132627116128986622556326754417184'
+      },
+      bls: {
+        hex: '86b50179774296419b7e8375118823ddb06940d9a28ea045ab418c7ecbe6da84d416cb55406eec6393db97ac26e38bd4',
+        x: '1032310052213111954219872643110557886857426355660880187884944929197921151497879875809567553053909278931474809326548',
+        y: '864098652078652984775100613126389130872250925668471516116602988297282146682432492413990258728259241305725439599101',
+        z: '1'
+      },
       isLoading: false
     };
   }
@@ -35,26 +56,30 @@ class ECCCalculator extends React.Component {
     if (validity.valid) this.setPrivateKey(value);
   }
 
-  calculateKeys() {
+  async calculateKeys() {
     const priv = this.state.privateKey;
     console.log('Calculating keys');
-    console.time('secp256k1'); console.time('ed25519'); console.time('bls12');
-
     const privateKey = priv.replace(/^0x/, '');
 
-    const secpPub = secp256k1.getPublicKey(privateKey, {isCompressed: true});
-    console.timeEnd('secp256k1');
+    const [secpPub, edPub, blsPub] = await Promise.all([
+      Promise.resolve(secp256k1.getPublicKey(privateKey, true)).then(timer('secp256k1 key')),
+      ed25519.getPublicKey(privateKey).then(timer('ed25519 key')),
+      Promise.resolve(bls12.getPublicKey(privateKey)).then(timer('bls key'))
+    ]);
+    const sp = secp256k1.Point.fromHex(secpPub);
+    const ep = ed25519.Point.fromHex(edPub);
+    const bp = bls12.PointG1.fromCompressedHex(blsPub);
 
-    // this.setState({secpPub, isLoading: false});
-    ed25519.getPublicKey(privateKey).then(edPub => {
-      console.timeEnd('ed25519');
-      const blsPub = arrayToHex(bls12.getPublicKey(privateKey));
-      console.timeEnd('bls12');
-      this.setState({secpPub, edPub, blsPub, isLoading: false});
-      if (this.state.message) {
-        this.setState({isSigning: true, message: this.state.message});
-      }
-    })
+    const pubs = {
+      secp: {hex: secpPub, x: sp.x.toString(), y: sp.y.toString()},
+      ed: {hex: edPub, x: ep.x.toString(), y: ep.y.toString()},
+      bls: {hex: blsPub, x: bp.x.value.toString(), y: bp.y.value.toString(), z: bp.z.value.toString()},
+      isLoading: false
+    }
+    this.setState(pubs);
+    if (this.state.message) {
+      this.setState({isSigning: true, message: this.state.message});
+    }
   }
 
   onSign(event) {
@@ -64,22 +89,20 @@ class ECCCalculator extends React.Component {
   async calculateSignatures() {
     const msg = this.state.message;
     const priv = this.state.privateKey;
+    const msgb = new TextEncoder().encode(msg);
     const privateKey = priv.replace(/^0x/, '');
-    const message = new TextEncoder().encode(msg);
-    console.log('Signing');
-    console.time('secp256k1'); console.time('ed25519'); console.time('bls12');
-
-    const ed = await ed25519.sign(msg, privateKey);
-    console.timeEnd('ed25519');
-    const secp = secp256k1.sign(message, privateKey);
-    console.timeEnd('secp256k1');
-    const bls = await bls12.sign(message, privateKey, 1);
-    console.timeEnd('bls12');
-
+    const hash = arrayToHex(await ed25519.utils.sha512(msgb));
+    const [secpSign, secpSchnorrSign, edSign, blsSign] = await Promise.all([
+      secp256k1.sign(hash, privateKey).then(timer('secp256k1 sign')),
+      secp256k1.schnorr.sign(hash, privateKey).then(timer('secp256k1 schnorr sign')),
+      ed25519.sign(hash, privateKey).then(timer('ed25519 sign')),
+      bls12.sign(arrayToHex(msgb), privateKey).then(timer('bls sign'))
+    ]);
     this.setState({
-      edSign: ed,
-      secpSign: arrayToHex(secp),
-      blsSign: arrayToHex(bls),
+      edSign,
+      secpSign,
+      secpSchnorrSign,
+      blsSign,
       isSigning: false
     });
   }
@@ -105,9 +128,29 @@ class ECCCalculator extends React.Component {
 
       <h3>Public keys {this.state.isLoading && <div className="lds-hourglass"></div>}</h3>
       <ul>
-        <li class="ed"><code>{this.state.edPub}</code></li>
-        <li class="secp"><code>{this.state.secpPub}</code></li>
-        <li class="bls"><code>{this.state.blsPub}</code></li>
+        <li class="secp">
+          <code>{this.state.secp.hex}</code>
+          <ul>
+            <li>x = <code>{this.state.secp.x}</code></li>
+            <li>y = <code>{this.state.secp.y}</code></li>
+          </ul>
+        </li>
+        <li class="ed">
+          <code>{this.state.ed.hex}</code>
+          <ul>
+            <li>x = <code>{this.state.ed.x}</code></li>
+            <li>y = <code>{this.state.ed.y}</code></li>
+          </ul>
+        </li>
+        <li class="bls">
+          <code>{this.state.bls.hex}</code>
+          <ul>
+            <li><small>G1 Projective coordinates</small></li>
+            <li>x = <code>{this.state.bls.x}</code></li>
+            <li>y = <code>{this.state.bls.y}</code></li>
+            <li>z = <code>{this.state.bls.z}</code></li>
+          </ul>
+        </li>
       </ul>
 
       <div class="ecc-signatures">
@@ -116,8 +159,9 @@ class ECCCalculator extends React.Component {
         <p><textarea onChange={this.onSign.bind(this)}></textarea></p>
 
         <ul>
-          <li class="ed"><code>{this.state.edSign}</code></li>
           <li class="secp"><code>{this.state.secpSign}</code></li>
+          <li class="secp-schnorr"><code>{this.state.secpSchnorrSign}</code></li>
+          <li class="ed"><code>{this.state.edSign}</code></li>
           <li class="bls"><code>{this.state.blsSign}</code></li>
         </ul>
       </div>
